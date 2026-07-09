8f45adeafffda4cad41d8da02c0e8b9d597c17cb
CleanDev-Fix <219162456+CleanDev-Fix@users.noreply.github.com>
2026-07-08 21:42:07 -0400

    Polyfill Blob for Node 16 Jest

--- jest.setup.js
+++ jest.setup.js
@@ -1,7 +1,12 @@
 const webStreams = require('stream/web');
+const { Blob } = require('buffer');
 
 for (const name of ['ReadableStream', 'TransformStream', 'WritableStream']) {
   if (typeof globalThis[name] === 'undefined' && webStreams[name]) {
     globalThis[name] = webStreams[name];
   }
+}
+
+if (typeof globalThis.Blob === 'undefined' && Blob) {
+  globalThis.Blob = Blob;
 }
